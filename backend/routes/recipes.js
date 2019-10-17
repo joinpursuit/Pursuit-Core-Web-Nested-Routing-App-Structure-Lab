99e07dfff88da21b2a923a70a696edba5d1721c1
Suzette <suzetteislam@gmail.com>
2019-10-17 16:00:58 -0400

    user changes

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -8,7 +8,9 @@ const allRecipes = [
 
     },
     {
-        name: ''
+        name: 'Lasagna',
+        ingredients:['Pasta', 'tomato sauce', 'cheese','meat',],
+        directions: 'Cook beef in a large skillet over medium heat, stirring until it crumbles and is no longer pink; drain. Stir in pasta sauce. Spread one-third of meat sauce in a lightly greased 11- x 7-inch baking dish; layer with 3 noodles and half each of ricotta cheese and mozzarella cheese. (The ricotta cheese layers will be thin.) Repeat procedure; spread remaining one-third of meat sauce over mozzarella cheese. Slowly pour 1/4 cup hot water around inside edge of dish. Tightly cover baking dish with 2 layers of heavy-duty aluminum foil. Bake at 375° for 45 minutes.'
     }
 ];
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+const allUsers = [
+    {
+        username: "donna112",
+        email: "donna112@gmail.com",
+        activationDate: "January 1, 1970"
+    },
+    {
+        username: 'sislam12',
+        email: 'sislam12@binghamton.edu',
+        activationDate: 'July 5, 2012'
+    },
+    {
+        username: 'rosita',
+        email: 'rose@yahoo.com',
+        activationDate: 'October 31, 1998'
+    },
+    {
+        username: 'django90',
+        email: 'django90@gmail.com',
+        activationDate: 'December 5, 2015'
+    },
+    {
+        username: 'cupKate',
+        email: 'kac1234@gmail.com',
+        activationDate: 'May 24, 2005'
+    }
+]
 router.get('/all', (req,res) => {
-    res.json('users will be displayed here!')
-    console.log("test")
+    res.json(allUsers)
 })
 
 module.exports = router;
