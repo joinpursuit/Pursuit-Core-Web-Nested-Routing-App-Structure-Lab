28de3b99a2a134c42d913259ac86270b2ae0d84e
vonbarown <vonielbrown@pursuit.org>
2019-10-16 20:57:05 -0400

    expanding on the users enpoint

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,30 @@ router.post('/add', (req, res) => {
 router.delete('/delete', (req, res) => {
     let username = req.body.username;
 
+    res.send({
+        status: `Success operation successful`,
+        message: `${username} has been successfully deleted`
+    })
 })
+
+router.patch('/update', (req, res) => {
+    let user = req.body;
+    for (let i = 0; i < 10; i++) {
+        let userObj = {
+            username: user.username,
+            email: user.email,
+            activationDate: user.activationDate
+        }
+    }
+    res.json(userObj)
+})
+
+// const arr = usersArray
+// router.get('/all', (res, req) => {
+
+//     // usersArray.forEach(el => res.json(el))
+//     res.send(arr)
+// })
 
 
 module.exports = router
