3db1105cf87aee9fb0dc4f1aad41786b928bbf61
Jenesh <jenesh.napit@gmail.com>
2019-10-18 13:38:45 -0400

    Lab almost complete with one route incomplete and a return statement bug

--- middleware/users.js
+++ middleware/users.js
@@ -1,37 +1,51 @@
 class User {
     constructor() {
         this.users = [
-            {
-                    username: "user1234",
-                    email: "user1234@gmail.com",
-                    activationDate: "January 1, 1970"
-            }
-        ]
+        // {
+        //     username: "user1234",
+        //     email: "user1234@gmail.com",
+        //     activationDate: "January 1, 1970"
+        // }
+        ];
     }
 
     addUser = (username, email, activationDate) => {
+        let message = `The username '${username}' already exists. Therefore it was not added.`;
+        this.users.forEach(ele => {
+            console.log('User: ', ele.username, 'Adding: ', username);
+            console.log(ele.username === username)
+            if (ele.username == username) {
+                console.log('Duplicate found!')
+                return {message};
+            }
+        })
         this.users.push({username, email, activationDate});
-        return this.users;
+        message = `The username '${username}' was successfully added.`;
+        return {message, users: this.users};
     }
 
     getAllUsers = () => {
-        return this.users;
+        return {users: this.users};
     }
 
     updateUser = (username, email, activationDate) => {
+        let message = `Successfully added ${username}`;
         this.users.forEach((ele, i) => {
-            if (ele.username === username) {
-                let updatedUser = {username, email, activationDate};
+            if (ele.username == username) {
+                console.log('Found');
+                const updatedUser = {username, email, activationDate};
                 this.users.splice(i, 1, updatedUser);
+                return {message, users: this.users};
             }
         })
-        return this.users;
+        message = `The username '${username}' does not exist. Therefore it was not updated.`
+        return {message};
     }
 
     getAllActivation = (date) => {
         let returnArr = [];
         this.users.forEach(ele => {
-            if (ele.activationDate.includes(date))  {
+            if (ele.activationDate.includes(date)) {
                 returnArr.push(ele);
             }
         })
@@ -40,11 +54,13 @@ class User {
 
     deleteUser = username => {
         this.users.forEach((ele, i) => {
-            if (ele.username === username)  {
+            if (ele.username === username) {
                 this.users.splice(i, 1);
                 return this.users;
             }
         })
+        const message = `The username ${username} does not exist. Therefore nothing was deleted.`;
+        return {message};
     }
 }
 
