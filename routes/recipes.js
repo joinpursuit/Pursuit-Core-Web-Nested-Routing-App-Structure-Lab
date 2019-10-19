f48560366024c5afc5c4a68c1c484f7cfd50f9af
Bpmauge <briahanamauge@pursuit.org>
2019-10-19 19:03:13 -0400

    delete works for users and post works for recipes

--- routes/recipes.js
+++ routes/recipes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 let recipes = [
     {
-    name: 'Grilled Cheese',
-    ingredients: ['Bread', 'Cheese', 'Butter'],
-    directions: `Preheat skillet over medium heat. Generously butter one side of a slice of bread 
+        name: 'Grilled Cheese',
+        ingredients: ['Bread', 'Cheese', 'Butter'],
+        directions: `Preheat skillet over medium heat. Generously butter one side of a slice of bread 
     Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. 
     Butter a second slice of bread on one side and place butter-side-up on top of sandwich. 
     Grill until lightly browned and flip over; continue grilling until cheese is melted. 
@@ -14,24 +14,45 @@ let recipes = [
 ]
 
 
-router.get('/', (req, res)=> {
-    let newRecipe = req.body;
-    recipes.push(newRecipe);
-    res.json(recipes);
+router.get('/', (req, res) => {
+    let newRecipe = req.body.name;
+    if (!newRecipe) {
+        res.json(recipes)
+    } else {
+        recipes.push(newRecipe);
+        res.json(recipes);
+    }
 });
 
 router.post('/', (req, res) => {
-   let newRecipe = req.body;
-   recipes.push(newRecipe);
-   res.json(recipes);
+    let newRecipe = {
+        name: req.body.name,
+        ingredients: req.body.ingredients.split(', '),
+        directions: req.body.directions
+    } 
     
+    recipes.push(newRecipe);
+    res.json(recipes);
+    
+    // let newRecipe = req.body
+    // recipes.push(newRecipe);
+    // res.json(recipes);
+
 });
 
-router.delete('/:recipe_id', (req, res) => {
-    res.send(`where you remove a recipe`);
+router.delete('/delete/:recipe_id', (req, res) => {
+    let removeRecipe = req.params.name;
+
+    recipes.map(element => {
+        if (element.name === removeUser) {
+            let index = recipes.indexOf(element);
+            recipes.splice(index, 1);
+        }
+    });
+    res.send(recipes)
 });
 
-router.patch('/:recipe_id', (req, res) =>{
+router.patch('/:recipe_id', (req, res) => {
     res.send(`where you update a recipe`)
 })
 
--- routes/users.js
+++ routes/users.js
@@ -4,21 +4,21 @@ const router = express.Router();
 
 let users = [
     {
-    username: 'John Doe',
-    email: 'test@123.com',
-    activationDate: `January 1, 1970`
+        username: 'John Doe',
+        email: 'test@123.com',
+        activationDate: `January 1, 1970`
     }
 ]
 
 
 router.get('/', (req, res) => {
-   
+
     let newUser = req.body.username;
-    if (!newUser){
+    if (!newUser) {
         res.json(users)
-    }else{
-    users.push(newUser);
-    res.json(users);
+    } else {
+        users.push(newUser);
+        res.json(users);
     }
 });
 
@@ -26,44 +26,22 @@ router.post('/', (req, res) => {
     let newUser = req.body;
     users.push(newUser);
     res.json(users);
-    
+
 });
 
-router.delete(`/:username`, (req, res)=> {
-let removeUser = req.params.username; 
-let newUser = [];
+router.delete(`/delete/:username`, (req, res) => {
+    let removeUser = req.params.username;
 
-//first try
-/* if(users.includes(removeUser)){
- console.log(users.indexOf(removeUser))
- res.json(removeUser)
- } */
-
- //second try 
-/* for( let i=0; i<=users.length; i++){
-    if(users[i].username !== removeUser){
-        newUser.push(users[i]);
-     }
-     res.json(newUser)
- } */
-
-//third try
-/* console.log(users[users.username].indexOf(removeUser))
-     let removeUser = req.query.username;
-    if(newUser.username === removeUser){
-        console.log(newUser.username)
-    } */
-
-    //fourth try
-    /* let newUsers = users.filter(element => {
-        return element.name !== removeUser
-       });
-     res.status(removeUser).body(newUser)
-     console.log(`old ${users} and new ${newUsers}`)
-     console.log(removeUser)
-     res.send(`${removeUser} was removed and we only have ${newUsers} left`)
-*/
+    users.map(element => {
+        if (element.username === removeUser) {
+            let index = users.indexOf(element);
+            users.splice(index, 1);
+        }
+    });
+    res.send(users)
 });
+
+
 
 router.patch('/:user_id', (req, res) => {
     res.send(`where you update a user`);
