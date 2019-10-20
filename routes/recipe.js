0ca2350a534f72ad8eac3afe255d237bd47ab24d
kadjiah <kadijahwilson@pursuit.org>
2019-10-20 10:22:48 -0400

    deletepost

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-recipe = [{ 
-  recipe:   {
+recipe = [
+    { 
         name: "Grilled Cheese",
         ingredients: [
             "Bread",
@@ -12,7 +12,7 @@ recipe = [{
         directions: "Preheat skillet over medium heat. Generously butter one side of a slice of bread. Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese."
     }
 
-}]
+]
 
 router.get('/all', (req, res) => {
     res.json({
@@ -30,7 +30,7 @@ const validateInput = (req, res, next) =>{
     }else{
         next()
     }
-    console.log(recipe)
+    // console.log(recipe)
 }
 
 
@@ -54,21 +54,44 @@ const postRecipe = (req, res, next) => {
 
 router.post('/newRecipe', validateInput, postRecipe)
 
-const deleteRecipe = (req, res, next) => {
-    let recipeDelete = req.body;
-    let deletePost = recipeDelete.name
+// const deleteRecipe = (req, res, next) => {
+//     let recipeDelete = recipe;
+//     let deletePost = recipeDelete.name
 
-let empArr = []
-    if(deletePost){
-        res.send({
-            name: " ",
-            ingredients: empArr,
-            directions: " "
-        })
-    }
+//     // let empArr = []
+//     if(deletePost){
+//         res.send({
+//             // name: " ",
+//             // ingredients: empArr,
+//             // directions: " "
+    
+//         })
+//     next()
+// }
+// }
+
+const postDelete = (req,res) => {
+    let checkRecipe = recipe;
+   let param = req.body.name
+
+   const deleteRecipe = checkRecipe.filter(recipe => recipe.name !== param)
+
+// for(let i = 0; i < checkRecipe.length; i++){
+ 
+//     let recipeName =  checkRecipe[i].name
+
+//  if(recipeName === param){
+//     delete checkRecipe[i].name 
+//     delete checkRecipe[i].ingredients
+//     delete checkRecipe[i].directions
+//  }
+// }
+
+res.send(deleteRecipe)
 }
 
-router.delete('/deleteRecipe', deleteRecipe)
+
+router.delete('/deleteRecipe', postDelete )
 
 
-module.exports = router;
+module.exports = router
