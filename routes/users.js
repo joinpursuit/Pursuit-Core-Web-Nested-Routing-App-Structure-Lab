c1346441172c976301bba6f4c9b68a5abaaa41e7
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2019-10-23 14:58:34 -0400

    0.7, users patch & delete routes added

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,13 @@ const g = {
     numOfUsers: 0,
     nameToId: {},
     idToName: {},
+    nameToUsersIdx: {},
+    nextUsersIdxAssign: 1,
     data: [null]
+  },
+  changeAllowed: {
+    username: true,
+    email: true
   }
 };
 
@@ -29,7 +35,8 @@ class User {
   constructor (username, email, activeDate, id) {
     this.username = username;
     this.email = email;
-    this.activeDate = activeDate;
+    this.activeDate = activeDate || new Date();
+    this.index = g.usersJSON.nextUsersIdxAssign;
     if (id) {
       this.id = id;
     }
@@ -46,23 +53,138 @@ class User {
       if (g.usersJSON.idToName[proposedId]) {
         proposedId += 1;
       } else {
-        g.usersJSON.nameToId[el.username] = proposedId;
-        g.usersJSON.idToName[proposedId] = el.username;
         el.id = proposedId;
       }
     }
+    g.usersJSON.nameToId[el.username.toLowerCase()] = el.id;
+    g.usersJSON.idToName[el.id] = el.username.toLowerCase();
+    g.usersJSON.nameToUsersIdx[el.username.toLowerCase()] = g.usersJSON.nextUsersIdxAssign;
+    g.usersJSON.nextUsersIdxAssign += 1;
     g.usersJSON.data.push(el);
   }
 }
 
 
 /* HELPERS */
+const ignoreEmpties = (arr) => {
+  return arr.filter(el => !!el)
+}
 
 
 /* MIDDLEWARE */
+const checkExists = (req, res, next) => {
+  if (!g.usersJSON.nameToId[req.query.target.toLowerCase()]) {
+    res.json({
+        status: "fail",
+        message: "Error: cannot find the target account. Check the username and try again."
+    });
+  } else {
+    next();
+  }
+}
+
+const checkDupeName = (req, res, next) => {
+  if (g.usersJSON.nameToId[req.body.username.toLowerCase()]) {
+      res.json({
+          status: "fail",
+          message: "Error: username already taken. Please try another."
+      });
+  } else {
+    next();
+  }
+}
+
+const checkInputs = (req, res, next) => {
+  let problems = [];
+  for (let key in req.body) {
+    const str = req.body[key];
+    switch (key) {
+      case "username":
+        if (!str) {
+          problems.push('username missing');
+        } else if (str.length > 15) {
+          problems.push('username can\'t be longer than 15 characters');
+        }
+        break;
+      case "email":
+        if (!str) {
+          problems.push('email missing');
+        } else if (!str.includes('@') || !str.slice(-5).includes('.')) {
+          problems.push('please enter a valid email address');
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  if (problems[0]) {
+    if (problems.length >= 2) {
+      problems[problems.length - 1] = "and " + problems[problems.length - 1];
+      problems.length > 2
+        ? problems = problems.join(', ')
+        : problems = problems.join(' ');
+    } else {
+      problems = problems.join('');
+    }
+    res.json({
+        status: "fail",
+        message: `Error: ${problems}. Please fix your inputs and try again.`
+    });
+  } else {
+    next();
+  }
+}
+
+const patchUser = (req, res, next) => {
+  for (let key in req.body) {
+    if (g.changeAllowed[key]) {
+      g.usersJSON.data[g.usersJSON.nameToUsersIdx[req.query.target.toLowerCase()]][key] = req.body[key];
+    }
+  }
+  res.json({
+    status: "success",
+    message: `Account \'${req.query.target}\' has been updated.`
+});
+}
+
+const addUser = (req, res, next) => {
+  new User (
+    req.body.username,
+    req.body.email
+  );
+  res.json({
+      status: "success",
+      message: `New account \'${req.body.username}\' has been created.`
+  });
+}
+
+const delUser = (req, res, next) => {
+  const target = req.query.target.toLowerCase();
+  const deletedName = g.usersJSON.data[g.usersJSON.nameToUsersIdx[target]].username;
+  g.usersJSON.data.splice(g.usersJSON.nameToUsersIdx[target], 1, null);
+  g.usersJSON.numOfUsers -= 1;
+  // nameToId: {},
+  // idToName: {},
+  // nameToUsersIdx: {},
+  delete g.usersJSON.nameToUsersIdx[target];
+  delete g.usersJSON.idToName[g.usersJSON.nameToId[target]];
+  delete g.usersJSON.nameToId[target];
+  res.json({
+      status: "success",
+      message: `\'${deletedName}\' user has been removed.`
+  });
+}
 
 
 /* ROUTES */
+router.post("/", checkDupeName, checkInputs, addUser);
+router.patch("/edit", checkExists, checkInputs, patchUser);
+router.delete("/edit", checkExists, delUser);
+router.get("/filter", searchUsers);
+router.get("/all", (req, res) => res.json(ignoreEmpties(g.usersJSON.data)));
+
+// unpublished route for debugging
+router.get("/json", (req, res) => res.json(g.usersJSON));
 
 
 /* TEMP DATA POPULATION */ // TODO move to separate file
@@ -72,12 +194,9 @@ for (let i = 1; i < moreUsers.length; i++) {
     moreUsers[i].username,
     moreUsers[i].email,
     moreUsers[i].activeDate,
-    moreUsers[i].id
-      ? moreUsers[i].id
-      : undefined
+    moreUsers[i].id || undefined
   )
 }
-log(g.usersJSON.data);
 
 
 module.exports = router;
