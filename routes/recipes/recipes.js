e76457412f49399fe5c85d62d55a2f2e3fd18ebd
cassidybeni <cassidybeni@pursuit.org>
2020-01-12 16:28:16 -0500

    completed the users route

--- routes/recipes/recipes.js
+++ routes/recipes/recipes.js
@@ -1,9 +1,31 @@
 const recipes = require("express").Router()
 
+let recipesArr = [
+    {
+    name: "Grilled Cheese",
+    ingredients: [
+        "Bread",
+        "Cheese",
+        "Butter"
+    ],
+    directions: "Preheat skillet over medium heat. Generously butter one side of a slice of bread. Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese."
+    },
+    {
+        name: "Classic Peanut butter & Jelly Sandwich",
+        ingredients: [
+            "Bread",
+            "Peanut Butter",
+            "Strawberry Jelly"
+        ],
+        directions: "Toast the bread. Spread peanut butter one side of a slice of bread and jelly on the other slice of the bread. Place spreaded peanut butter on top of the spreaded jelly. Bon appetit"
+        }
+]
 
 
 
-
+recipes.get("/", (req, res) => {
+    res.json(recipesArr)
+})
 
 
 
--- routes/users/users.js
+++ routes/users/users.js
@@ -1,6 +1,42 @@
 const users = require("express").Router()
 
+let usersArr = [
+    {
+    username: "user1234",
+    email: "user1234@gmail.com",
+    activationDate: "January 1, 2010"
+    },
+    {
+    username: "user4321",
+    email: "user4321@yahoo.com",
+    activationDate: "December 21, 2019"
+    }
+]
 
+users.get("/", (req, res) => {
+    res.json(usersArr)
+})
+
+const addUser = (req, res, next) => {
+    usersArr.push(req.body)
+    res.json(usersArr)
+    next()
+}
+
+users.post("/newUsers", addUser, (req, res) => {
+    res.json(req.body)
+})
+
+users.delete("/:username", (req, res) => {
+    let username = req.params.username
+    usersArr.splice(username, 1)
+    res.json(usersArr)
+})
+
+users.put("/:id", (req, res) => {
+    usersArr[req.params.id] = req.body;
+    res.json(usersArr[req.params.id])
+})
 
 
 module.exports = users
