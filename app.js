63b300d6c2cf764c94a676a72d57eef633984a04
crystal cardona <crystalcardona@pursuit.org>
2020-01-12 16:18:27 -0500

    working on ingredients

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 
-const express = requires("express");
-const cors = requires("cors");
-const bodyParser = requires("body-parser");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const recipeRouter = require("./routes/recipe/recipe.js")
 
-let recipeObj = {
+app.use("/recipe", recipeRouter)
+app.use(cors());
+
+let recipeObj = [{
     name: "Grilled Cheese",
     ingredients: [
         "Bread",
@@ -13,30 +17,30 @@ let recipeObj = {
         "Butter"
     ],
     directions: "Preheat skillet over medium heat. Generously butter one side of a slice of bread. Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese."
-}
+}]
 
 
-app.get("/recipe", (req, res) => {
-    res.json("all recipes")
-})
+// app.get("/recipe", (req, res) => {
+//     res.json("all recipes")
+// })
 
-app.get("/recipe/:id", (req, res) => {
-    res.json("Specific recipe")
-})
+// app.get("/recipe/:id", (req, res) => {
+//     res.json("Specific recipe")
+// })
 
-app.delete("/recipe/:id", (req, res) => {
-    res.json("Delete Recipe")
-})
+// app.delete("/recipe/:id", (req, res) => {
+//     res.json("Delete Recipe")
+// })
 
-app.post("/recipe/", (req, res) => {
-    res.json("Created new recipe")
-})
+// app.post("/recipe/", (req, res) => {
+//     res.json("Created new recipe")
+// })
 
-const hasIngredient = (req, res, next) => {
-    if(recipeObj["ingredients"].includes(req.params)){
-        res.json()
-    }
-}
-app.get("/")
+// const hasIngredient = (req, res, next) => {
+//     if(recipeObj["ingredients"].includes(req.params)){
+//         res.json()
+//     }
+// }
+
 
 app.listen(port, () => "listening")
--- routes/recipe/recipe.js
+++ routes/recipe/recipe.js
@@ -1,7 +1,28 @@
 const recipe = require("express").Router();
 
-recipe.get("/", (req, res) => {
-    res.json("All recipes")
+
+let recipeObj = {
+    name: "Grilled Cheese",
+    ingredients: [
+        "Bread",
+        "Cheese",
+        "Butter"
+    ],
+    directions: "Preheat skillet over medium heat. Generously butter one side of a slice of bread. Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese."
+}
+
+
+
+
+recipe.get("/:ingredients/", (req, res) => {
+    let ingredient = req.params.ingredients
+        recipeObj.ingredients.forEach(el => {
+            if(el === ingredient){
+                console.log(el.name)
+            } else {
+                res.json("This ingredient is not available")
+            }
+        })
 })
 
 recipe.post("/", (req, res) => {
@@ -12,9 +33,9 @@ recipe.delete("/", (req, res) => {
     res.json("Delete Recipe")
 })
 
-recipe.get("/:id", (req, res) => {
+recipe.get("/:name", (req, res) => {
     res.json("Specific Recipe")
 })
 
-module.export = recipe ;
+module.exports = recipe ;
 
