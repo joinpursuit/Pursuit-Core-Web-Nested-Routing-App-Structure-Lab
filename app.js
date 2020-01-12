294a88b308db39a18c15a1200b7f31a2536fb592
Ohidurr <Ohidurrahman96@gmail.com>
2020-01-12 16:15:08 -0500

    finish part1

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('bodyParser')
+const bodyParser = require('body-parser')
 const port = 3000;
 const app = express()
 const userRouter = require('./routes/users/users.js')
 const recipeRouter = require('./routes/recipes/recipes.js')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 app.use(cors())
 
 app.use("/users",userRouter)
