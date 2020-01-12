30c0bc773b9f36dc1d4b8597b6659e1f046c9c1c
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-12 17:26:59 -0500

    finished may tweek a few things

--- recipes.js
+++ recipes.js
@@ -1,25 +1,25 @@
 const recipes = require("express").Router()
-
+let allRecipes = [{
+    name:"Grilled Cheese",
+    ingredients:["bread", "butter", "cheese"],
+    directions: "Preheat skillet over medium heat. Generously butter one side of a slice of bread. Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese."
+},
+{
+    name:"Peanut Butter and Jelly",
+    ingredients:["bread", "Peanut Butter", "Jelly"],
+    directions: "Spread the peanut butter on one piece of bread.Spread the jelly on the other side. Put the two pieces of bread together to form a sandwich. Toddler adaptation: cut off crusts before serving"
+},
+{
+    name:"Buttered Toast",
+    ingredients:["bread", "butter"],
+    directions: "Generously butter one side of a slice of bread."
+}]
 recipes.get("/", (req, res)=>{
-    res.json([{
-        name:"Grilled Cheese",
-        ingredients:["bread", "butter", "cheese"],
-        directions: "Preheat skillet over medium heat. Generously butter one side of a slice of bread. Place bread butter-side-down onto skillet bottom and add 1 slice of cheese. Butter a second slice of bread on one side and place butter-side-up on top of sandwich. Grill until lightly browned and flip over; continue grilling until cheese is melted. Repeat with remaining 2 slices of bread, butter and slice of cheese."
-    },
-    {
-        name:"Peanut Butter and Jelly",
-        ingredients:["bread", "Peanut Butter", "Jelly"],
-        directions: "Spread the peanut butter on one piece of bread.Spread the jelly on the other side. Put the two pieces of bread together to form a sandwich. Toddler adaptation: cut off crusts before serving"
-    },
-    {
-        name:"Buttered Toast",
-        ingredients:["bread", "butter"],
-        directions: "Generously butter one side of a slice of bread."
-    },])
-    res.json("All Recipes received")
+
+    res.json(allRecipes)
 })
 
-recipes.post("/", (req, res)=>{
+recipes.post("/newRecipes", (req, res)=>{
     let newRecipe ={
         name:req.params.name,
         ingredients:req.params.ingredients,
@@ -37,15 +37,15 @@ recipes.post("/", (req, res)=>{
     })
     
 
-recipes.patch("/:recipeId", (req, res)=>{
+recipes.patch("/:recipe", (req, res)=>{
     res.json("Updated recipe" + req.params.id)
 })
 
-recipes.delete("/:recipeId", (req, res)=>{
+recipes.delete("/:name", (req, res)=>{
     let element = parseInt(req.params.id)
     let arr 
-    for(let i = 0; i< user.data; i++){
-        if(element === users.data[i].id){
+    for(let i = 0; i< allRecipes.data; i++){
+        if(element === this.allRecipes.data[i].id){
             arr = recipes.data[i]
             recipes.data.splice(i,1)
         }
@@ -56,7 +56,11 @@ recipes.delete("/:recipeId", (req, res)=>{
 
 
 recipes.get("/:ingredient", (req, res)=>{
-    res.json("Recipes with " + ingredient + req.params.id)
+    let ingredient = req.params.ingredient;
+    let output = allRecipes.filter(recipe => {
+        return recipe.ingredients.includes(ingredient)
+    })
+    res.json(output)
 })
 
 module.exports = recipes
--- users.js
+++ users.js
@@ -1,6 +1,22 @@
 const users = require("express").Router()
 
-const allUsers = 
+const allUsers = [{
+    name:"John",
+    id: 1,
+    email:"johnsmith@gmail.com",
+    dateActivated: "January 1, 2015"
+},{
+name:"Jane",
+id:2,
+email:"janedoe@gmail.com",
+dateActivated: "July 15, 2019"
+},
+{
+    name:"Bob",
+    id:3,
+    email:"bobbrown@gmail.com",
+    dateActivated: "March 30, 2011"
+    }]
 users.post("/", (req, res)=>{
     let newUser ={
         name:req.params.name,
@@ -39,27 +55,15 @@ users.patch("/:userChange", (req, res)=>{
 })
 
 users.get("/", (req, res)=>{
-    res.json([{
-        name:"John",
-        id: 1,
-        email:"johnsmith@gmail.com",
-        dateActivated: "January 1, 2015"
-    },{
-    name:"Jane",
-    id:2,
-    email:"janedoe@gmail.com",
-    dateActivated: "July 15, 2019"
-    },
-    {
-        name:"Bob",
-        id:3,
-        email:"bobbrown@gmail.com",
-        dateActivated: "March 30, 2011"
-        }])
+res.json(allUsers)
 })
 
 users.get("/dateActivated", (req, res)=>{
-   res.json("Activation Date" + req.params.id)
+    let date = req.params.date;
+    let output = allUsers.filter(user => {
+        return user.dateActivated.includes(date)
+    })
+    res.json(output)
 })
 
 module.exports = users
